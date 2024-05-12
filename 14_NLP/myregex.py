8469b844c809cec98746cc41e8b3f3633c573725
mkib1212 <158325423+mkib1212@users.noreply.github.com>
2024-05-12 18:26:42 +0200

    Add files via upload

---
+++ 14_NLP/myregex.py
@@ -0,0 +1,20 @@
+""" 
+\d : digit 0-9
+\D : not digit
+
+\w: word a-z A-Z, 0-9, _
+\W: not a word (dot, new line, tab, space)
+"""
+
+
+
+
+text_to_search = """
+ABCDEF
+12345
+abcdef
+
+Mr. Mohamed
+Mrs. Zimmermann
+
+"""
